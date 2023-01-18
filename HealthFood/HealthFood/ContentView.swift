08fc344f9773610563989298456145eef9cf45b9
Mpabasara <miththampi@gmail.com>
2023-01-18 16:38:37 +0530

    init + app created

---
+++ HealthFood/HealthFood/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HealthFood
+//
+//  Created by Mobios on 1/18/23.
+//  Copyright © 2023 Pabasara. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
